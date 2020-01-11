cd3da2c24b9b3dc5eacc872bff835b8785afdd3c
viktoriiakrokis <vika.krokis063@gmail.com>
2020-01-11 14:19:09 +0200

    first commit

---
+++ INTRO-TO-JSX/Attributes-in-JSX.jsx
@@ -0,0 +1,14 @@
+// The value should be wrapped in quotes, like this:
+my-attribute-name="my-attribute-value"
+
+// Here are some JSX elements with attributes:
+<a href="http://www.example.com">Welcome to the Web</a>;
+
+const title = <h1 id="title">Introduction to React.js: Part I</h1>; 
+
+// A single JSX element can have many attributes, just like in HTML:
+const panda = <img src="images/panda.jpg" alt="panda" width="500px" height="500px" />;
+
+// exercise
+const p1 = <p id="large">foo</p>;
+const p2 = <p id="small">bar</p>;
